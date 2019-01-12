a8758b63aeed48fd9f24608fb45b1b177dfbbbf7
Raj Sagar <therajsagar@gmail.com>
2019-01-12 23:47:52 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => <h1>Welcome to Hacker News Client!</h1>
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
