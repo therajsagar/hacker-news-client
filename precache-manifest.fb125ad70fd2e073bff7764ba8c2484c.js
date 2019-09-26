7dce36e64b03720d1f7699ac913b474568a890b9
Raj Sagar <therajsagar@gmail.com>
2019-09-26 11:55:39 +0530

    Updates

---
+++ precache-manifest.fb125ad70fd2e073bff7764ba8c2484c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b415900227a0fad4a4e19de415f240",
+    "url": "/hacker-news-client/index.html"
+  },
+  {
+    "revision": "499b50f3d6d65da7e369",
+    "url": "/hacker-news-client/static/css/main.47c7589c.chunk.css"
+  },
+  {
+    "revision": "245395f504f17ec64411",
+    "url": "/hacker-news-client/static/js/2.b4e7a58b.chunk.js"
+  },
+  {
+    "revision": "499b50f3d6d65da7e369",
+    "url": "/hacker-news-client/static/js/main.54eb548c.chunk.js"
+  },
+  {
+    "revision": "a92ae0a0e99dff83190a",
+    "url": "/hacker-news-client/static/js/runtime-main.fd55451c.js"
+  },
+  {
+    "revision": "0b07ad7bbc718676148b48f83176440c",
+    "url": "/hacker-news-client/static/media/bug.0b07ad7b.gif"
+  }
+]);
